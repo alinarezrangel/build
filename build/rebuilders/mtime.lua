b7343370eaffc7a5ab4e84d7db862466693603b6
Alejandro Linarez Rangel <alinarezrangel@gmail.com>
2022-08-31 14:06:26 -0500

    Fix bug in `mtime` rebuilder if any of the inspected files didn't exist.

--- build/rebuilders/mtime.lua
+++ build/rebuilders/mtime.lua
@@ -7,12 +7,16 @@ return function(Posix_File_System)
             local deps = task:get_dependencies()
             local my_mtime = Posix_File_System.get_mtime(fs, key)
             local out_of_date = false
-            for i = 1, #deps do
-               local dep_key = deps[i]
-               local dep_mtime = Posix_File_System.get_mtime(fs, dep_key)
-               if dep_mtime > my_mtime then
-                  out_of_date = true
-                  break
+            if not my_mtime then
+               out_of_date = true
+            else
+               for i = 1, #deps do
+                  local dep_key = deps[i]
+                  local dep_mtime = Posix_File_System.get_mtime(fs, dep_key)
+                  if not dep_mtime or dep_mtime > my_mtime then
+                     out_of_date = true
+                     break
+                  end
                end
             end
             if out_of_date or #deps == 0 then
